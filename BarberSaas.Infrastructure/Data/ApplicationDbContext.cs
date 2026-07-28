8504cc06af76633bc0286abb01e7b5679fd9459f
Kaua Menezes <kauamenezes6@gmail.com>
2026-07-27 23:32:32 -0300

    configuring entityframework for the appointments db

---
+++ BarberSaas.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BarberSaas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarberSaas.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Appointment>().ToTable("Appointments");
+        }
+
+    }
+}
