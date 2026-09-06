369eb0d07273d0a59df3747d169531871ecb211c
Kaua Menezes <kauamenezes6@gmail.com>
2026-09-06 12:50:47 -0300

    working on new repositories

---
+++ BarberSaas.Domain/Repositories/IBarberShopRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using BarberSaas.Domain.Entities;
+
+namespace BarberSaas.Domain.Repositories
+{
+    public interface IBarbershopRepository
+    {
+        Task AddAsync(Barbershop barbershop);
+        Task<Barbershop> GetByIdAsync(Guid id);
+        Task UpdateAsync(Barbershop barbershop);
+    }
+}
---
+++ BarberSaas.Infrastructure/Repositories/BarberShopRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using BarberSaas.Domain.Entities;
+using BarberSaas.Domain.Repositories;
+using BarberSaas.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberSaas.Infrastructure.Repositories
+{
+    public class BarberShopRepository : IBarbershopRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BarberShopRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Barbershop barbershop)
+        {
+            await _context.Barbershops.AddAsync(barbershop);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Barbershop> GetByIdAsync(Guid id)
+        {
+            return await _context.Barbershops.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task UpdateAsync(Barbershop barbershop)
+        {
+            _context.Barbershops.Update(barbershop);
+            await _context.SaveChangesAsync();
+        }
+    }
+} 
